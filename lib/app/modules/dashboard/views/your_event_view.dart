bc5bd1c459cbbf864a93019ff5d8c03bba5a333c
m26ikhsanmaulana26 <m26ikhsanmaulana26@gmail.com>
2025-01-16 04:37:34 +0000

    belajar flutter part-6

--- lib/app/modules/dashboard/views/your_event_view.dart
+++ lib/app/modules/dashboard/views/your_event_view.dart
@@ -7,6 +7,7 @@ import 'add_view.dart';
 import 'edit_view.dart';
 
 class YourEventView extends GetView {
+  // event
   const YourEventView({super.key});
   @override
   Widget build(BuildContext context) {
